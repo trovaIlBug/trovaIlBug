e0b2d12cf74016b4ad592902bc836cd03147957b
Gaetano Smario <gaetano.smario@liparipeople.com>
2024-03-23 20:26:58 +0100

    Inizialized project

---
+++ src/main/java/findTheBug/FindTheBugApplication.java
@@ -0,0 +1,13 @@
+package findTheBug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindTheBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindTheBugApplication.class, args);
+	}
+
+}

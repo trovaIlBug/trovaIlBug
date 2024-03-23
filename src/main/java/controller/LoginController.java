144c871fe568437203eaed6f9ece2d1456f9e8b3
Gaetano Smario <gaetano.smario@liparipeople.com>
2024-03-23 20:32:53 +0100

    Inizialized project

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,4 +1,14 @@
 package controller;
 
-public class LoginController {
+import it.be.api.login.LoginApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController implements LoginApi {
+
+    @Override
+    public ResponseEntity<String> testGet() {
+        return ResponseEntity.ok("ciao");
+    }
 }
